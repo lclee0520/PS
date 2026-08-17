df8b909bf313b61f8bcebcfaa89e61c7b99d87b4
lclee0520 <lc_lee0520@naver.com>
2026-08-18 02:27:52 +0900

    Clean up solution.cpp with using namespace std

--- Level1/두 정수 사이의 합/solution.cpp
+++ Level1/두 정수 사이의 합/solution.cpp
@@ -1,7 +1,8 @@
 #include <algorithm>
+using namespace std;
 
 long long solution(int a, int b) {
-    long long min_val = std::min(a, b);
-    long long max_val = std::max(a, b);
+    long long min_val = min(a, b);
+    long long max_val = max(a, b);
     return (min_val + max_val) * (max_val - min_val + 1) / 2;
 }
