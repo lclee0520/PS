83587fbe5849f6a8392c843d4ec52ef1abeb8bf7
lch <leech5728@gmail.com>
2026-09-06 17:15:19 +0900

    Add clarifying comments (JadenCase 문자열 만들기, 12951)

--- Level2/12951/solution.cpp
+++ Level2/12951/solution.cpp
@@ -6,14 +6,14 @@ string solution(string s) {
 
     transform(s.begin(), s.end(), s.begin(), ::tolower);
 
-    if(s[0] <= 'z' && s[0] >= 'a'){
+    if(s[0] <= 'z' && s[0] >= 'a'){ // 사실 이거 필요없음
         answer += toupper(s[0]);
     } else {
         answer += s[0];
     }
 
     for(int i = 1;i<s.length() ;i++){
-        if(s[i] == ' ' && s[i + 1] >= 'a' && s[i + 1] <= 'z'){
+        if(s[i] == ' ' && s[i + 1] >= 'a' && s[i + 1] <= 'z'){// 다음꺼 비교하지말고 그냥 toupper바로 하면 알아서 구분됨
             answer += s[i];
             answer += toupper(s[i + 1]);
             i++;
